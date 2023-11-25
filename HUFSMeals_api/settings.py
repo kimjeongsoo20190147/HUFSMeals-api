344af338d7a2593a8604c2336c5a4afcc17a22cb
ShinSub98 <wjdtjq26@naver.com>
2023-11-25 21:08:46 +0900

    refactor: set TIME_ZONE to korea

--- HUFSMeals_api/settings.py
+++ HUFSMeals_api/settings.py
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -139,3 +139,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
