8c53b206e139c13de9b554b58a1dee638c106747
ShinSub98 <wjdtjq26@naver.com>
2023-11-25 21:07:37 +0900

    feat: add User model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    google_id = models.BigIntegerField(unique=True, null = True)
+    nickname = models.CharField(max_length=100, null = True)
+    country = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nickname
