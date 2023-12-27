9ca9f4403b06f859e816f23a7559a6e51a08f095
ShinSub98 <wjdtjq26@naver.com>
2023-12-28 06:24:40 +0900

    feat: add menu model

--- restaurant/models.py
+++ restaurant/models.py
@@ -11,3 +11,12 @@ class Restaurant(models.Model):
     phone = models.CharField(max_length = 50)
     review_cnt = models.IntegerField(default = 0)
     score_avg = models.FloatField(default = 0)
+
+
+def menu_images_path(instance, filename):
+    return f'menu_images/{instance.restaurant.pk}'
+
+class Menu(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete = models.CASCADE)
+    name = models.CharField(max_length = 50)
+    image = models.ImageField(upload_to = menu_images_path, blank = True, null = True)
