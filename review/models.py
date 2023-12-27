23d36130fb09e3aa29e3a7010a09b21580d9c0b7
ShinSub98 <wjdtjq26@naver.com>
2023-12-28 06:28:04 +0900

    refactor: refact review model

--- review/models.py
+++ review/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from accounts.models import User
+from restaurant.models import Restaurant
 
 def user_images_path(instance, filename):
     return f'user_images/{instance.user.pk}'
 
 class Review(models.Model):
     user = models.ForeignKey(User, null = True, on_delete = models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, null = True, on_delete = models.CASCADE)
     rate = models.IntegerField(default= 0)
     image = models.ImageField(upload_to = user_images_path, blank = True, null = False)
     title = models.CharField(max_length = 100)
