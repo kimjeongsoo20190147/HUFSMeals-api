0a1122beb872246d039d422af0c7c1a12636f9d6
ShinSub98 <wjdtjq26@naver.com>
2023-12-28 13:45:51 +0900

    refactor: refactor review model

--- review/models.py
+++ review/models.py
@@ -8,10 +8,11 @@ def user_images_path(instance, filename):
 class Review(models.Model):
     user = models.ForeignKey(User, null = True, on_delete = models.CASCADE)
     restaurant = models.ForeignKey(Restaurant, null = True, on_delete = models.CASCADE)
-    rate = models.IntegerField(default= 0)
-    image = models.ImageField(upload_to = user_images_path, blank = True, null = False)
     title = models.CharField(max_length = 100)
     body = models.TextField(default = "")
-
+    src_lang = models.CharField(max_length = 10, null = True)
+    created_at = models.DateField(auto_now_add = True, null = True)
+    score = models.IntegerField(default= 0)
+    
     def __str__(self):
         return self.title
